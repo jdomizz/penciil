2b7d5b7d4299a370b373508004310b624fa5b09e
jdomizz <jdomidiaz@gmail.com>
2021-05-16 01:01:49 +0200

    adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,48 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import { VitePWA } from 'vite-plugin-pwa'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter(),
+		vite: {
+			plugins: [
+				VitePWA({
+					registerType: 'autoUpdate',  
+					manifest: {
+						name: 'Penciil',
+						short_name: 'Penciil',
+						description: 'A simple ASCII art sketchpad',
+						display: 'standalone',
+						orientation: 'any',
+						start_url: '.',
+						theme_color: '#ffffff',
+						background_color: '#ffffff',
+						icons: [
+							{
+								src: '/pwa-192x192.png',
+								sizes: '192x192',
+								type: 'image/png',
+							},
+							{
+								src: '/pwa-512x512.png',
+								sizes: '512x512',
+								type: 'image/png',
+							},
+							{
+								src: '/pwa-512x512.png',
+								sizes: '512x512',
+								type: 'image/png',
+								purpose: 'any maskable',
+							},
+						],
+					}
+				})
+			]
+		}
 	}
 };
 
