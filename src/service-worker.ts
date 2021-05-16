08b2f7b97b78b23e81d73cf3f836a945dd089c59
jdomizz <jdomidiaz@gmail.com>
2021-05-16 17:54:10 +0200

    sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -4,9 +4,6 @@ export type {}; // needed because of tsc --isolatedModules flag
 
 declare const self: ServiceWorkerGlobalScope;
 
-const cacheName = "penciil-cache";
-const version = "v1";
-
-self.addEventListener("install", (event) => null);
-self.addEventListener("activate", (event) => null);
-self.addEventListener("fetch", (event) => null);
+self.addEventListener("install", () => null);
+self.addEventListener("activate", () => null);
+self.addEventListener("fetch", () => null);
